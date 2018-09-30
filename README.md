9cdc16fb97e85b9b530a94a434c0692d6587f884
Jeff Langballe <jeffreynlangballe@gmail.com>
2018-09-30 18:34:03 -0400

    Add CLI usage for schmitt.py

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Some helper scripts for circuits
 Finds n closest PVNS pairs to create specified ratio
 Ratio must be between 0 and 1
 Usage: `python pvns.py <r1/r2 ratio> <number of results to display>`
+
+# schmitt.py
+Calculates min/max hysterisis voltage and resistor values for
+inverting Schmitt trigger circuit
+Usage: `python schmitt.py <r1> <r2> <r3> <tolerance> <v_ref>`
+Where r1, r2, r3 are nominal resistor values specified tolerance
--- schmitt.py
+++ schmitt.py
@@ -1,6 +1,12 @@
 """
-Functions and program to make math less annoying sensors LAB
+Calculates min/max hysterisis voltage and resistor values for
+inverting Schmitt trigger circuit
+Usage: `python schmitt.py <r1> <r2> <r3> <tolerance> <v_ref>`
+Where r1, r2, r3 are nominal resistor values specified tolerance
 """
+
+import sys
+
 
 def schmitt_v_lo(v_ref, r1, r2, r3):
     return v_ref / ((r2/r1) + (r2/r3) + 1)
@@ -37,8 +43,26 @@ def get_extreme_schmitt_hysterisis(v_ref, resistances, tolerance):
     return(min_params, max_params)
 
 if __name__ == '__main__':
-    resistances = [20000, 10000, 10000]
-    tolerance = 0.05
-    v_ref = 5
+    if len(sys.argv) < 6:
+        sys.exit(-1)
+
+    # Read input paraargumentsms
+    resistances = [float(r) for r in sys.argv[1:4]]
+    tolerance = float(sys.argv[4])
+    v_ref = float(sys.argv[5])
+
+    # Calculate voltages and resistor values
     params = get_extreme_schmitt_hysterisis(v_ref, resistances, tolerance)
-    print(params)
+
+    # Print output to console
+    print('Minimum hysterisis values:')
+    print('V range\t\t', params[0][0])
+    print('V_tl\t\t', params[0][1])
+    print('V_th\t\t', params[0][2])
+    print('R1, R2, R3\t', params[0][3])
+
+    print('\nMaximum hysterisis values:')
+    print('V range\t\t', params[1][0])
+    print('V_tl\t\t', params[1][1])
+    print('V_th\t\t', params[1][2])
+    print('R1, R2, R3\t', params[1][3])
